a9f3d178f724e1476b02115f7daa070be0545de7
unknown <hzhang98@usc.edu>
2024-11-03 16:13:16 -0800

    support branch

--- pc.v
+++ pc.v
@@ -1,7 +1,7 @@
 module program_counter (
     input clk,
     input reset,
-    input branch,
+    input branch_en,
     input [31:0] branch_target,
     output reg [31:0] ins_address
 );
@@ -9,7 +9,7 @@ module program_counter (
     always @(posedge clk) begin 
         if (reset) begin
             ins_address <= 32'b0;
-        end else if (branch) begin
+        end else if (branch_en) begin
             ins_address <= branch_target;
         end else begin
             ins_address <= ins_address + 4;
